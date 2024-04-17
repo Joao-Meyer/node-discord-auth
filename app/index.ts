7375b6cc96b73b0e43dca41e084ba2ab44c0ed97
italo <italo.ignacio@sp.senai.br>
2024-04-17 16:30:59 -0300

    fasdjkd

--- app/index.ts
+++ app/index.ts
@@ -1,4 +1,3 @@
-import { createServer } from 'http';
 import { env, setupMiddleware, setupRoutes } from '@main/config';
 import cors from 'cors';
 import express from 'express';
@@ -11,8 +10,6 @@ app.use(cors());
 
 setupRoutes(app);
 
-const http = createServer(app);
-
-http.listen(env.API.PORT, () => {
+app.listen(env.API.PORT, () => {
   console.info(`Server started at http://localhost:${env.API.PORT}`);
 });
