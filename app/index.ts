0a8e8645579a07e853e62d937c62d04ce7a5e5f0
italo <italo.ignacio@sp.senai.br>
2024-04-17 16:32:06 -0300

    dsa

--- app/index.ts
+++ app/index.ts
@@ -1,14 +1,20 @@
-import { env, setupMiddleware, setupRoutes } from '@main/config';
+import { env } from '@main/config';
 import cors from 'cors';
 import express from 'express';
 
 const app = express();
 
-setupMiddleware(app);
-
 app.use(cors());
 
-setupRoutes(app);
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/about', (req, res) => {
+  res.json({
+    ola: 'About route 🎉 '
+  });
+});
 
 app.listen(env.API.PORT, () => {
   console.info(`Server started at http://localhost:${env.API.PORT}`);
