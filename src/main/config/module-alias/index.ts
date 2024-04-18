66c1ce6184eed117aa0da75af1502db0d124f487
italo <italo.ignacio@sp.senai.br>
2024-04-18 08:05:14 -0300

    testeteds

--- src/main/config/module-alias/index.ts
+++ src/main/config/module-alias/index.ts
@@ -1,10 +1,8 @@
-/* eslint-disable no-ternary */
-import 'dotenv/config';
 import { addAlias } from 'module-alias';
 import { readdirSync } from 'fs';
 import { resolve } from 'path';
 
-const rootPath = typeof process.env.TS_NODE_DEV === 'undefined' ? 'dist' : 'dist';
+const rootPath = 'src';
 
 const layers = readdirSync(resolve(__dirname, '..', '..', '..', '..', rootPath));
 
