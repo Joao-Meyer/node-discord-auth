dd54139fafd116cc0579583be508fd58f4de85cd
italo <italo.ignacio@sp.senai.br>
2024-04-18 09:04:00 -0300

    bgb

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { app } from './main/config/app';
+import { env } from './main/config/env';
 
-app.listen(3000, () => {
-  console.info(`Server started at http://localhost:${3000}`);
+app.listen(env.API.PORT, () => {
+  console.info(`Server started at http://localhost:${env.API.PORT}`);
 });
--- src/main/config/app/index.ts
+++ src/main/config/app/index.ts
@@ -1,14 +1,8 @@
-import { setupMiddleware } from '../middleware';
-import { setupRoutes } from '../routes';
 import cors from 'cors';
 import express from 'express';
 
 const app = express();
 
-setupMiddleware(app);
-
 app.use(cors());
-
-setupRoutes(app);
 
 export { app };
