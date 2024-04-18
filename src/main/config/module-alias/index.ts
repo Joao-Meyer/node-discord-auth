28f1714bfe639d478d240a48a7cff93929ee479a
italo <italo.ignacio@sp.senai.br>
2024-04-18 08:00:55 -0300

    teste 2

--- src/main/config/module-alias/index.ts
+++ src/main/config/module-alias/index.ts
@@ -4,7 +4,7 @@ import { addAlias } from 'module-alias';
 import { readdirSync } from 'fs';
 import { resolve } from 'path';
 
-const rootPath = typeof process.env.TS_NODE_DEV === 'undefined' ? 'src' : 'src';
+const rootPath = typeof process.env.TS_NODE_DEV === 'undefined' ? 'dist' : 'dist';
 
 const layers = readdirSync(resolve(__dirname, '..', '..', '..', '..', rootPath));
 
