11e24f61512bb3d27705481dbc9137211fc0aa81
italo <italo.ignacio@sp.senai.br>
2024-04-18 08:57:47 -0300

    cdcd

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,6 @@
-import { DataSource } from './infra/database';
 import { env } from './main/config/env';
-import { errorLogger } from './main/utils';
+import { http } from './main/config/app';
 
-DataSource.$connect()
-  .then(async () => {
-    if (typeof env.API.PORT === 'string') {
-      const { http } = await import('./main/config/app');
-
-      http.listen(env.API.PORT, () => {
-        console.info(`Server started at http://localhost:${env.API.PORT}`);
-      });
-    } else console.info('Environment variables missing');
-  })
-  .catch((error: unknown) => {
-    if (error instanceof Error) {
-      console.error(`An error of type ${error.name} occurred. See the logs error...`);
-      console.error(error);
-    }
-    errorLogger(error);
-  });
+http.listen(env.API.PORT, () => {
+  console.info(`Server started at http://localhost:${env.API.PORT}`);
+});
